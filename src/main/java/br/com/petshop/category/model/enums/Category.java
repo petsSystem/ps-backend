2beea2268a3e131022dc25b2ee03197218bb3314
Vanessa <varronche@pedepronto.com.br>
2024-02-11 19:52:08 -0300

    update migrations

--- src/main/java/br/com/petshop/category/model/enums/Category.java
+++ src/main/java/br/com/petshop/category/model/enums/Category.java
@@ -6,7 +6,7 @@ import java.util.stream.Stream;
 
 @Getter
 public enum Category {
-    PETCARE("Cuidados", "Cuidados para o pet"),
+    PETCARE("Petshop", "Cuidados para o pet"),
     PETVET("Veterinária", "Serviços veterinários"),
     PETFOOD("Alimentação", "Alimentação para pets de estimação"),
     PETSERV("Serviços", "Outros tipos de serviços à disposição");
